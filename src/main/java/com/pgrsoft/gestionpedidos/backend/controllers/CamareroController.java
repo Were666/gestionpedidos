61b12be34bd8f4fb1a7a8b68ccde8631c9b1599b
jalemany <jordia4@gmail.com>
2018-10-02 12:09:08 +0200

    se añade POJO Producto

--- src/main/java/com/pgrsoft/gestionpedidos/backend/controllers/CamareroController.java
+++ src/main/java/com/pgrsoft/gestionpedidos/backend/controllers/CamareroController.java
@@ -1,7 +1,6 @@
 package com.pgrsoft.gestionpedidos.backend.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/com/pgrsoft/gestionpedidos/backend/model/Categoria.java
@@ -0,0 +1,5 @@
+package com.pgrsoft.gestionpedidos.backend.model;
+
+public enum Categoria {
+	CAFE, COMIDA, POSTRE, REFRESCO, LICOR, CERVEZA;
+}
