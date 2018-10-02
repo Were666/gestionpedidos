f6a666d86d5f1990368738b217c14e00a2e5a9ac
jalemany <jordia4@gmail.com>
2018-10-02 12:32:05 +0200

    schema.sql y data.sql para tabla Productos

--- src/main/resources/data/h2/data.sql
+++ src/main/resources/data/h2/data.sql
@@ -2,3 +2,9 @@ INSERT INTO CAMAREROS VALUES
 (100,'Pepín Gálvez Ridruejo'),
 (101,'Honorio Martín Salvador'),
 (102,'Anna Vílchez Andrés');
+
+INSERT INTO PRODUCTOS VALUES
+(1,'café solo', 1.20, 'Descripción Producto A','2015-10-20 00:00:00',false,'CAFE'),
+(2,'café con leche', 1.40, 'Descripción Producto B','2015-10-20 00:00:00',false,'CAFE'),
+(3,'café cortado', 1.30, 'Descripción Producto C','2015-10-20 00:00:00',false,'CAFE'),
+(4,'plato combinado', 8.00, 'Descripción Producto D','2015-10-20 00:00:00',false,'COMIDA'),
