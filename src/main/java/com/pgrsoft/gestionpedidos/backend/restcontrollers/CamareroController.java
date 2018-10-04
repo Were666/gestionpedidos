013635d2b483fd7084a197dda1a6692368bd3ee5
jalemanyco <jordia4@gmail.com>
2018-10-04 22:58:06 +0200

    metodo POST en CamareroController

--- src/main/java/com/pgrsoft/gestionpedidos/backend/restcontrollers/CamareroController.java
+++ src/main/java/com/pgrsoft/gestionpedidos/backend/restcontrollers/CamareroController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +22,14 @@ public class CamareroController {
 	@Autowired
 	private CamareroPresentationServices camareroPresentationServices;
 	
-	@RequestMapping(value="/{id}",
-					method=RequestMethod.GET,
-					produces=MediaType.APPLICATION_JSON_VALUE)
+	
+	// 	*****************************************************************************
+	//	GET BY ID
+	// 	*****************************************************************************
+	
+	@RequestMapping(value = "/{id}",
+					method = RequestMethod.GET,
+					produces = MediaType.APPLICATION_JSON_VALUE)
 	public CamareroVO getById(@PathVariable("id") Long id) {
 	
 		CamareroVO camarero = null;
@@ -37,12 +43,18 @@ public class CamareroController {
 		return camarero;
 	}
 	
-	@RequestMapping(value="",
-			method=RequestMethod.GET,
-			produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<CamareroVO>> dameString() {
+	
+	// 	*****************************************************************************
+	//	GET ALL
+	// 	*****************************************************************************
+	
+	@RequestMapping(value = "",
+					method = RequestMethod.GET,
+					produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<CamareroVO>> getAll() {
 
 		List<CamareroVO> camareros = null;
+		
 		try {
 			camareros = camareroPresentationServices.getAll();
 		} catch (Exception e) {
@@ -51,5 +63,28 @@ public class CamareroController {
 
 		return new ResponseEntity<List<CamareroVO>>(camareros, HttpStatus.OK);
 	}
+	
+	
+	// 	*****************************************************************************
+	//	CREATE
+	// 	*****************************************************************************
+	
+	@RequestMapping(value = "",
+					method = RequestMethod.POST,
+					consumes = MediaType.APPLICATION_JSON_VALUE,
+					produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<CamareroVO> create(@RequestBody CamareroVO camareroVO) {
+
+		CamareroVO createdCamarero = null;
+		
+		try {
+			createdCamarero = camareroPresentationServices.create(camareroVO);
+		} catch (Exception e) {
+			//TODO
+		}
+
+		return new ResponseEntity<CamareroVO>(createdCamarero, HttpStatus.OK);
+	}
+	
 	
 }
