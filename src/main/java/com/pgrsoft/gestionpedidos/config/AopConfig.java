5cbb07be5fdb14db379206a47c557e3a15c97c25
jalemanyco <jordia4@gmail.com>
2018-10-04 22:02:03 +0200

    cambio de nombre clase AopConfig

--- src/main/java/com/pgrsoft/gestionpedidos/config/AopConfig.java
+++ src/main/java/com/pgrsoft/gestionpedidos/config/AopConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Aspect
-public class ControllersAccessAspectConfig {
+public class AopConfig {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
