dc05dfc624ac4303e48730f6f259f1fa20205fca
jalemany <jordia4@gmail.com>
2018-10-09 10:04:50 +0200

    AppErrorController ahora devuelve un JSON

--- src/main/java/com/pgrsoft/gestionpedidos/backend/restcontrollers/AppErrorController.java
+++ src/main/java/com/pgrsoft/gestionpedidos/backend/restcontrollers/AppErrorController.java
@@ -12,27 +12,79 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppErrorController implements ErrorController {
 
 	@RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public ErrorDTO handleError(HttpServletRequest request) {
 		
 		Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		
+		ErrorDTO errorDTO = new ErrorDTO();
+		
 		if (statusCode != null) {
 			
+			errorDTO.setCodigo(statusCode);
+			
 	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
-	            return ":-( error-404";
+	        	
+	        	errorDTO.setMensaje("Recurso no encontrado...");
+	        
+	        	return errorDTO;
 	        }
 	        
 	        if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-	            return ":-( error-500";
+	        	
+	        	errorDTO.setMensaje(":-( error en el servidor. Estaría bien saber qué...");
+	        	
+	        	return errorDTO;
+	        
 	        }
 	    }
 		
-	    return "error";
+		return errorDTO;
+	
     }
 	
 	@Override
 	public String getErrorPath() {
 		return "/error";
+	}
+	
+	/**
+	 * 
+	 * Clase interna que modeliza un JSON de respuesta de error.
+	 * 
+	 * @author Jordi Alemany
+	 *
+	 */
+	
+	class ErrorDTO {
+		
+		private int codigo;
+		private String mensaje;
+		
+		public ErrorDTO() {
+			
+		}
+		
+		public ErrorDTO(int codigo, String mensaje) {
+			this.codigo = codigo;
+			this.mensaje = mensaje;
+		}
+
+		public int getCodigo() {
+			return codigo;
+		}
+
+		public void setCodigo(int codigo) {
+			this.codigo = codigo;
+		}
+
+		public String getMensaje() {
+			return mensaje;
+		}
+
+		public void setMensaje(String mensaje) {
+			this.mensaje = mensaje;
+		}
+		
 	}
 
 }
